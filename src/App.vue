a80b0352ac734954b7ac219201fc4e5291517545
Illia Komissarov <iliakomissarow99@gmail.com>
2023-11-16 20:21:26 +0200

    app changes

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,30 @@
-<script setup>
-import ParcelDetails from './components/ParcelDetails.vue'
-import Forme from './components/Forme.vue'
-</script>
-
 <template>
- <ParcelDetails/>
- <Forme/>
+  <div>
+    <h1>Parcel Transport App</h1>
+    <Forme
+      v-model:originCity="formData.fromCity"
+      v-model:destinationCity="formData.toCity"
+      v-model:parcelType="formData.parcelType"
+      v-model:dispatchDate="formData.dispatchDate"
+      v-model:parcelDescription="formData.parcelDescription"
+      @submit="createRequest"
+    />
+  </div>
 </template>
 
-<style scoped>
-</style>
+<script setup>
+import Forme from './components/Forme.vue';
+import { ref } from 'vue';
+
+const formData = ref({
+  fromCity: '',
+  toCity: '',
+  parcelType: '',
+  dispatchDate: '',
+  parcelDescription: '',
+});
+
+const createRequest = () => {
+  // Тут буде логіка для створення запиту
+};
+</script>
